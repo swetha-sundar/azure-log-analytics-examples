c59240104ddf9624d24ca2c28c473c81a791727d
Swetha Sundar <swetha.sundr@gmail.com>
2020-06-21 19:14:33 +0800

    Update the deployment logs data to be published

--- src/push_logs.py
+++ src/push_logs.py
@@ -9,59 +9,43 @@ from constants import Constants
 from env_var import Env
 
 e = Env()
-# Update the customer ID to your Log Analytics workspace ID
-# customer_id = 'xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx'
-
-# For the shared key, use either the primary or the secondary Connected Sources client authentication key
-#shared_key = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
 
 # The log type is the name of the event that is being submitted
-log_type = 'WebMonitorTest'
+log_type = "$(LogType)"
 
 # An example JSON web monitor object
 json_data = [{
-   "slot_ID": 12345,
-    "ID": "5cdad72f-c848-4df0-8aaa-ffe033e75d57",
-    "availability_Value": 100,
-    "performance_Value": 6.954,
-    "measurement_Name": "last_one_hour",
-    "duration": 3600,
-    "warning_Threshold": 0,
-    "critical_Threshold": 0,
-    "IsActive": "true"
-},
-{
-    "slot_ID": 67890,
-    "ID": "b6bee458-fb65-492e-996d-61c4d7fbb942",
-    "availability_Value": 100,
-    "performance_Value": 3.379,
-    "measurement_Name": "last_one_hour",
-    "duration": 3600,
-    "warning_Threshold": 0,
-    "critical_Threshold": 0,
-    "IsActive": "false"
-}]
+    "DeploymentID": "$(Release.DeploymentID)",
+    "ReleaseName": "$(Release.DefinitionName)",
+    "AgentName": "$(Agent.Name)",
+    "Time":"$CurrentTime",
+    "Action":"$(Action)",
+    "StoreName": "$(StoreName)"
+  }
+]
 body = json.dumps(json_data)
+print(body)
 
 # Build the API signature
-def build_signature(customer_id, shared_key, date, content_length, method, content_type, resource):
+def build_signature(la_workspace_id, shared_key, date, content_length, method, content_type, resource):
     x_headers = 'x-ms-date:' + date
     string_to_hash = method + "\n" + str(content_length) + "\n" + content_type + "\n" + x_headers + "\n" + resource
     bytes_to_hash = bytes(string_to_hash, encoding='utf8')
     decoded_key = base64.b64decode(shared_key)
     encoded_hash = base64.b64encode(hmac.new(decoded_key, bytes_to_hash, digestmod=hashlib.sha256).digest())
-    authorization = "SharedKey {}:{}".format(customer_id,encoded_hash)
+    authorization = "SharedKey {}:{}".format(la_workspace_id,encoded_hash)
     return authorization
 
 # Build and send a request to the POST API
-def post_data(customer_id, shared_key, body, log_type):
+def post_data(la_workspace_id, shared_key, body, log_type):
     method = 'POST'
     content_type = 'application/json'
     resource = '/api/logs'
     rfc1123date = datetime.datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')
     content_length = len(body)
-    signature = build_signature(customer_id, shared_key, rfc1123date, content_length, method, content_type, resource)
-    uri = 'https://' + customer_id + '.ods.opinsights.azure.com' + resource + '?api-version=2016-04-01'
+    signature = build_signature(la_workspace_id, shared_key, rfc1123date, content_length, method, content_type, resource)
+
+    uri = 'https://' + la_workspace_id + Constants.LOG_ANALYTICS_ENDPOINT + resource + '?api-version=2016-04-01'
 
     headers = {
         'content-type': content_type,
